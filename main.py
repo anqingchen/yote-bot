f30889f0c6cb693837be939d108ac91bcd707e61
anqingchen <benjamin.c0427@gmail.com>
2021-02-24 14:58:24 -0600

    stop replying to bots and add activity to show server count

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ client = discord.Client();
 
 @client.event
 async def on_ready():
-  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="yeets"))
+  client.user.
+  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f'yeets in {len(client.servers)} servers'))
 
 @client.event
 async def on_message(message):
+  if message.author.bot:
+    return
   if "yeet" in message.content.lower():
     await message.channel.send("yote")
 
